d03c5121a37e083af029532c767d18c3c7d77bfd
Luiz Ribas <lhs.ribas@gmail.com>
2020-09-16 19:50:15 -0300

    revision of comments, and inclusion of methods to SaveSession, UpdateSession, DeleteSession, FindSessionByID, FindSessionByWorkspaceID

--- repository/SessionRepoistory.go
+++ repository/SessionRepoistory.go
@@ -1,38 +1,96 @@
 package repository
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
+	"github.com/openpracticetool/maestro/converter"
 	"github.com/openpracticetool/maestro/model"
 )
 
-// SessionRepository struct
+// SessionRepository ::: struct
 type SessionRepository struct {
 	db *gorm.DB
 }
 
-// NewSessionRespository returns a new SessionRepository
+// NewSessionRespository ::: returns a new SessionRepository
 func NewSessionRespository(db *gorm.DB) *SessionRepository {
 	return &SessionRepository{
 		db: db,
 	}
 }
 
-// SaveSession save the Session data in database
-func (sr *SessionRepository) SaveSession(Session model.Session) {
-	//GetConnection().Create(&Session)
+// SaveSession ::: save the Session data in database
+func (sr *SessionRepository) SaveSession(session model.Session) (model.Session, error) {
+	// save the object session in database
+	db := sr.db.Save(&session)
+
+	// check with returned some error of procedure
+	if db.Error != nil {
+		return model.Session{}, db.Error
+	}
+
+	// convert the value returned to db for a session object
+	err := converter.ConverterInterfaceTOStruct(db.Value, &session)
+	return session, err
 }
 
-// UpdateSession update the Session data in database
-func (sr *SessionRepository) UpdateSession() {
+// UpdateSession ::: update the Session data in database
+func (sr *SessionRepository) UpdateSession(session model.Session) (model.Session, error) {
+	// updates the columns
+	db := sr.db.Model(&session).Where("id = ?", session.ID).UpdateColumns(model.Session{Name: session.Name, Description: session.Description, UpdatedBY: session.UpdatedBY, UpdatedAt: time.Now()})
+
+	// check with returned some error of procedure
+	if db.Error != nil {
+		return model.Session{}, db.Error
+	}
+
+	// convert the value returned to db for a session object
+	err := converter.ConverterInterfaceTOStruct(db.Value, &session)
+	return session, err
 
 }
 
-// DeleteSession delete the Session data in database
-func (sr *SessionRepository) DeleteSession() {
+// DeleteSession ::: delete the Session data in database
+func (sr *SessionRepository) DeleteSession(session model.Session, ID int) error {
+	// delete a session of database
+	db := sr.db.Delete(&session, ID)
 
+	return db.Error
 }
 
-// FindSessionByID find the Session by ID in database
-func (sr *SessionRepository) FindSessionByID() {
+// FindSessionByID ::: find the Session by ID in database
+func (sr *SessionRepository) FindSessionByID(ID int) (model.Session, error) {
+	// create a variable session
+	var session = model.Session{}
 
+	// find the session by ID
+	db := sr.db.First(&session, ID)
+
+	// check with returned some error of procedure
+	if db.Error != nil {
+		return model.Session{}, db.Error
+	}
+
+	// convert the value returned to db for a session object
+	err := converter.ConverterInterfaceTOStruct(db.Value, &session)
+	return session, err
+}
+
+// FindSessionByWorkspaceID ::: Find the Session by ID of Workspace
+func (sr *SessionRepository) FindSessionByWorkspaceID(workspaceID int) ([]model.Session, error) {
+	// create an array of sessions
+	var sessions = []model.Session{}
+
+	// find a session by ID of an workspace
+	db := sr.db.Where("id_workspace = ?", workspaceID).Find(&sessions)
+
+	// check with returned some error of procedure
+	if db.Error != nil {
+		return []model.Session{}, db.Error
+	}
+
+	// convert the value returned to db for a session object
+	err := converter.ConverterInterfaceTOStruct(db.Value, &sessions)
+	return sessions, err
 }
--- repository/WorkspaceRepository.go
+++ repository/WorkspaceRepository.go
@@ -8,43 +8,45 @@ import (
 	"github.com/openpracticetool/maestro/model"
 )
 
-// WorkspaceRepository struct data
+// WorkspaceRepository ::: struct
 type WorkspaceRepository struct {
 	db *gorm.DB
 }
 
-// NewWorkspaceRepository returns a new WorkspaceRepository
+// NewWorkspaceRepository ::: returns a new WorkspaceRepository
 func NewWorkspaceRepository(db *gorm.DB) *WorkspaceRepository {
 	return &WorkspaceRepository{
 		db: db,
 	}
 }
 
-// SaveWorkspace save the Workspace data in database
+// SaveWorkspace ::: save the Workspace data in database
 func (wr *WorkspaceRepository) SaveWorkspace(workspace model.Workspace) (model.Workspace, error) {
+	// save the workspace in database
 	db := wr.db.Save(&workspace)
 
+	// check with returned some error of procedure
 	if db.Error != nil {
 		return model.Workspace{}, db.Error
 	}
 
+	// convert the value returned to db for a workspace object
 	err := converter.ConverterInterfaceTOStruct(db.Value, &workspace)
 	return workspace, err
 }
 
-// UpdateWorkspace update the workspace data in database
+// UpdateWorkspace ::: update the workspace data in database
 func (wr *WorkspaceRepository) UpdateWorkspace(workspace model.Workspace) (model.Workspace, error) {
-	// updates the conlumns od database
+	// updates the columns
 	db := wr.db.Model(&workspace).Where("id = ? ", workspace.ID).UpdateColumns(model.Workspace{Description: workspace.Description, Name: workspace.Name, UpdatedBY: workspace.UpdatedBY, UpdatedAt: time.Now()})
 
-	// checks if database return errror
+	// check with returned some error of procedure
 	if db.Error != nil {
 		return model.Workspace{}, db.Error
 	}
 
-	// convert the interface response in struct
+	// convert the value returned to db for a workspace object
 	err := converter.ConverterInterfaceTOStruct(db.Value, &workspace)
-
 	return workspace, err
 }
 
@@ -57,30 +59,37 @@ func (wr *WorkspaceRepository) DeleteWorkspace(ID int) error {
 	return db.Error
 }
 
-// FindWorkspaceByID find the workspace by ID in database
+// FindWorkspaceByID ::: find the workspace by ID in database
 func (wr *WorkspaceRepository) FindWorkspaceByID(ID int) (model.Workspace, error) {
+	// create a variable wrokspace
 	var workspace = model.Workspace{}
 
-	// convert the interface response in struct
-	err := converter.ConverterInterfaceTOStruct(wr.db.First(&workspace, ID).Value, &workspace)
+	// find the workspace by ID
+	db := wr.db.First(&workspace, ID)
 
+	// check with returned some error of procedure
+	if db.Error != nil {
+		return model.Workspace{}, db.Error
+	}
+	// convert the value returned to db for a workspace object
+	err := converter.ConverterInterfaceTOStruct(db.Value, &workspace)
 	return workspace, err
 }
 
-// FindWorkspaceByCreatedBy find the workspaces by user creation
+// FindWorkspaceByCreatedBy ::: find the workspaces by user creation
 func (wr *WorkspaceRepository) FindWorkspaceByCreatedBy(createdBy string) ([]model.Workspace, error) {
 	// initializes an array
 	var workspaces = []model.Workspace{}
+
 	// consulting in the database with the createdby parameter
 	db := wr.db.Where("created_by = ?", createdBy).Find(&workspaces)
 
-	// check if the db return error
+	// check with returned some error of procedure
 	if db.Error != nil {
 		return workspaces, db.Error
 	}
 
-	// convert the interface response in struct
+	// convert the value returned to db for an array of workspace
 	err := converter.ConverterInterfaceTOStruct(db.Value, &workspaces)
-
 	return workspaces, err
 }
